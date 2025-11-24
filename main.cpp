71bd9944814a18266a3909bdef961ef746f2bfa1
Fernando Diaz <fernandodiasfd780518@gmail.com>
2025-11-24 06:52:33 -0300

    novo teste

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
 #include<iostream>
-int main(){
+struct color(){
+
+virtual int x() const=0;
+virtual int y()const=0;
+virtual int abs_sqr()const=0;
+virtual ~plano()=default;
+
 }
+
+planar*make(std::istcam& is)
+planar*mostleft(planar** pls, size_t k);
+int main() {
+
+planar* pls*[10]={}
+size_t k=0;
+pls[k+]=make(std::cin);
+drow(left);
+free_planar(pls,k);
+}
+
